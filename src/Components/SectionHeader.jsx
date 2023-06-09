10ef3f17fd49555ba539c0c76fbf172dcf1278d7
golam-kibriya123 <golamkibriya622@gmail.com>
2023-06-09 08:05:37 -0500

    added popular class

---
+++ src/Components/SectionHeader.jsx
@@ -0,0 +1,13 @@
+import underline from "../assets/Logo/PNG/under-line.png"
+const SectionHeader = () => {
+    return (
+        <div className="relative my-5 w-full flex flex-col items-center ">
+            <h1 className="text-2xl text-primary  p-2">
+                Popular Classes
+            </h1>
+            <img src={underline} alt="" className=" absolute w-40 -bottom-2" />
+        </div>
+    );
+};
+
+export default SectionHeader;
--- src/Pages/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -1,8 +1,80 @@
+import SectionHeader from '../../../Components/SectionHeader'
+import popular1 from '../../../assets/popular/c1.jpeg'
+import popular2 from '../../../assets/popular/c2.jpeg'
+import popular3 from '../../../assets/popular/c3.jpeg'
+import popular4 from '../../../assets/popular/c4.jpeg'
+import popular5 from '../../../assets/popular/c5.jpeg'
+import popular6 from '../../../assets/popular/c6.jpeg'
+
 
 const PopularClasses = () => {
     return (
         <div>
-PopularClasses
+          <SectionHeader></SectionHeader>
+            <div className='grid grid-cols-3 gap-10 p-10'>
+
+                <div className="card w-96 glass">
+                    <figure><img src={popular1} alt="car!" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Life hack</h2>
+                        <p>How to park your car at your garage?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Learn now!</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 glass">
+                    <figure><img src={popular2} alt="car!" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Life hack</h2>
+                        <p>How to park your car at your garage?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Learn now!</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 glass">
+                    <figure><img src={popular3} /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Life hack</h2>
+                        <p>How to park your car at your garage?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Learn now!</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 glass">
+                    <figure><img src={popular4} alt="car!" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Life hack</h2>
+                        <p>How to park your car at your garage?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Learn now!</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 glass">
+                    <figure><img src={popular5} alt="car!" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Life hack</h2>
+                        <p>How to park your car at your garage?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Learn now!</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 glass">
+                    <figure><img src={popular6} alt="car!" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Life hack</h2>
+                        <p>How to park your car at your garage?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Learn now!</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
