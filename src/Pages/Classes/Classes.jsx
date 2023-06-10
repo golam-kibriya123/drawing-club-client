acc53e251f44409d8afe5dd1c3f275189b14f0ff
golam-kibriya123 <golamkibriya622@gmail.com>
2023-06-09 22:41:46 -0500

    added aos

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -1,14 +1,24 @@
 import SectionHeader from "../../Components/SectionHeader";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import { useEffect } from "react";
 
 const Classes = () => {
+    useEffect(() => {
+        // ..
+        AOS.init();
+    }, [])
     return (
         <div>
             <SectionHeader header={'Classes'}>
             </SectionHeader>
             <div className="p-10 space-y-8">
 
-
-                <div className="card card-side bg-red-500 bg-opacity-40  shadow-xl border border-primary flex flex-col md:flex-row ">
+                <div className="card card-side bg-red-500 bg-opacity-40  shadow-xl border border-primary flex flex-col md:flex-row "
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-offset="200"
+                >
                     <figure>
                         <img src="https://media.istockphoto.com/id/1183183783/photo/female-artist-works-on-abstract-oil-painting-moving-paint-brush-energetically-she-creates.jpg?s=612x612&w=0&k=20&c=JLPrSmpdzPklAVKycBJ83oPASPfFPS46XvN0TShfLwI=" alt="Movie" className="rounded-2xl w-[100%] -z-10" />
                     </figure>
@@ -25,7 +35,15 @@ const Classes = () => {
                 </div>
 
 
-                <div className="card card-side bg-red-500 bg-opacity-40  shadow-xl border border-primary flex flex-col md:flex-row ">
+
+
+
+
+                <div className="card card-side bg-red-500 bg-opacity-40  shadow-xl border border-primary flex flex-col md:flex-row "
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-offset="200"
+                >
                     <figure>
                         <img src="https://media.istockphoto.com/id/1183183783/photo/female-artist-works-on-abstract-oil-painting-moving-paint-brush-energetically-she-creates.jpg?s=612x612&w=0&k=20&c=JLPrSmpdzPklAVKycBJ83oPASPfFPS46XvN0TShfLwI=" alt="Movie" className="rounded-2xl w-[100%] -z-10" />
                     </figure>
@@ -42,7 +60,11 @@ const Classes = () => {
                 </div>
 
 
-                <div className="card card-side bg-red-500 bg-opacity-40  shadow-xl border border-primary flex flex-col md:flex-row ">
+                <div className="card card-side bg-red-500 bg-opacity-40  shadow-xl border border-primary flex flex-col md:flex-row "
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-offset="200"
+                >
                     <figure>
                         <img src="https://media.istockphoto.com/id/1183183783/photo/female-artist-works-on-abstract-oil-painting-moving-paint-brush-energetically-she-creates.jpg?s=612x612&w=0&k=20&c=JLPrSmpdzPklAVKycBJ83oPASPfFPS46XvN0TShfLwI=" alt="Movie" className="rounded-2xl w-[100%] -z-10" />
                     </figure>
@@ -59,7 +81,11 @@ const Classes = () => {
                 </div>
 
 
-                <div className="card card-side bg-red-500 bg-opacity-40  shadow-xl border border-primary flex flex-col md:flex-row ">
+                <div className="card card-side bg-red-500 bg-opacity-40  shadow-xl border border-primary flex flex-col md:flex-row "
+
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-offset="200">
                     <figure>
                         <img src="https://media.istockphoto.com/id/1183183783/photo/female-artist-works-on-abstract-oil-painting-moving-paint-brush-energetically-she-creates.jpg?s=612x612&w=0&k=20&c=JLPrSmpdzPklAVKycBJ83oPASPfFPS46XvN0TShfLwI=" alt="Movie" className="rounded-2xl w-[100%] -z-10" />
                     </figure>
@@ -76,7 +102,11 @@ const Classes = () => {
                 </div>
 
 
-                <div className="card card-side bg-red-500 bg-opacity-40  shadow-xl border border-primary flex flex-col md:flex-row ">
+                <div className="card card-side bg-red-500 bg-opacity-40  shadow-xl border border-primary flex flex-col md:flex-row "
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-offset="200"
+                >
                     <figure>
                         <img src="https://media.istockphoto.com/id/1183183783/photo/female-artist-works-on-abstract-oil-painting-moving-paint-brush-energetically-she-creates.jpg?s=612x612&w=0&k=20&c=JLPrSmpdzPklAVKycBJ83oPASPfFPS46XvN0TShfLwI=" alt="Movie" className="rounded-2xl w-[100%] -z-10" />
                     </figure>
@@ -93,7 +123,11 @@ const Classes = () => {
                 </div>
 
 
-                <div className="card card-side bg-red-500 bg-opacity-40  shadow-xl border border-primary flex flex-col md:flex-row ">
+                <div className="card card-side bg-red-500 bg-opacity-40  shadow-xl border border-primary flex flex-col md:flex-row "
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-offset="200"
+                >
                     <figure>
                         <img src="https://media.istockphoto.com/id/1183183783/photo/female-artist-works-on-abstract-oil-painting-moving-paint-brush-energetically-she-creates.jpg?s=612x612&w=0&k=20&c=JLPrSmpdzPklAVKycBJ83oPASPfFPS46XvN0TShfLwI=" alt="Movie" className="rounded-2xl w-[100%] -z-10" />
                     </figure>
@@ -110,7 +144,11 @@ const Classes = () => {
                 </div>
 
 
-                <div className="card card-side bg-red-500 bg-opacity-40  shadow-xl border border-primary flex flex-col md:flex-row ">
+                <div className="card card-side bg-red-500 bg-opacity-40  shadow-xl border border-primary flex flex-col md:flex-row "
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-offset="200"
+                >
                     <figure>
                         <img src="https://media.istockphoto.com/id/1183183783/photo/female-artist-works-on-abstract-oil-painting-moving-paint-brush-energetically-she-creates.jpg?s=612x612&w=0&k=20&c=JLPrSmpdzPklAVKycBJ83oPASPfFPS46XvN0TShfLwI=" alt="Movie" className="rounded-2xl w-[100%] -z-10" />
                     </figure>
